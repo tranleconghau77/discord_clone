b2f2b343ab1ffc2a697a796c6ca537bda970ce5f
Hau Tran <tranleconghau772001@gmail.com>
2024-03-19 06:30:14 +0700

    initial profile

---
+++ app/(setup)/page.tsx
@@ -0,0 +1,5 @@
+const SetupPage = async () => {
+  return <>Create a Server</>;
+};
+
+export default SetupPage;
--- lib/db.ts
+++ lib/db.ts
@@ -3,5 +3,5 @@ declare global {
   var prisma: PrismaClient | undefined;
 }
 
-const db = new PrismaClient();
+export const db = new PrismaClient();
 if (process.env.NODE_ENV !== "production") globalThis.prisma = db;
