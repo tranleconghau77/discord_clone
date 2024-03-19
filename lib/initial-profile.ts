990e7d7a38f1f3b5717345923cb27c6dd0e0ddda
Hau Tran <tranleconghau772001@gmail.com>
2024-03-19 23:10:10 +0700

    prisma run

--- lib/initial-profile.ts
+++ lib/initial-profile.ts
@@ -1,7 +1,6 @@
 import { currentUser, redirectToSignIn } from "@clerk/nextjs";
 
 import { db } from "@/lib/db";
-import { use } from "react";
 
 export const initialProfile = async () => {
   const user = await currentUser();
@@ -10,7 +9,7 @@ export const initialProfile = async () => {
     return redirectToSignIn();
   }
 
-  const profile = db.profile.findUnique({
+  const profile = await db.profile.findUnique({
     where: { userId: user.id },
   });
 
@@ -20,6 +19,10 @@ export const initialProfile = async () => {
     data: {
       userId: user.id,
       name: `${user.firstName} ${user.lastName}`,
+      imageUrl: user.imageUrl,
+      email: user.emailAddresses[0].emailAddress,
     },
   });
+
+  return newProfile;
 };
