293c9526fc11b21df90e6ea9bebfe76fe2950fa5
Hau Tran <tranleconghau772001@gmail.com>
2024-03-21 22:44:02 +0700

    use zod

--- components/modals/initial-modal.tsx
+++ components/modals/initial-modal.tsx
@@ -1,5 +1,9 @@
 "use client";
 
+import * as z from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+
 import {
   DialogHeader,
   Dialog,
@@ -8,7 +12,26 @@ import {
   DialogDescription,
 } from "../ui/dialog";
 
+const formSchema = z.object({
+  name: z.string().min(1, {
+    message: "Server name is required",
+  }),
+  imageUrl: z.string().min(1, {
+    message: "Server name is required",
+  }),
+});
+
 export const InitialModal = () => {
+  const form = useForm({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: "",
+      imageUrl: "",
+    },
+  });
+
+  const isLoading = form.formState.isSubmitting;
+
   return (
     <Dialog open>
       <DialogContent className="bg-white text-black p-0 overflow-hidden">
@@ -16,7 +39,7 @@ export const InitialModal = () => {
           <DialogTitle className="text-2xl text-center font-bold">
             Customize your server
           </DialogTitle>
-          <DialogDescription className="text-center text-zinc">
+          <DialogDescription className="text-center text-zinc-500">
             Give your server a personality with a name and an image. You can
             change it later.
           </DialogDescription>
